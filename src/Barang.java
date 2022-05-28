e2c01f8f336347b171c85ce6e82c1c60b0588638
DannSama <axedrill.13@gmail.com>
2022-05-28 13:07:02 +0800

    ini masih test

--- src/Barang.java
+++ src/Barang.java
@@ -1,4 +1,40 @@
 public class Barang {
     private String nama;
-    
+    private double harga;
+    private int stock;
+
+    public Barang(String nama, double harga, int stock) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stock = stock;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public void dispSt(){
+        System.out.println("nama barang : "+nama);
+        System.out.println("harga barang : "+harga);
+        System.out.println("stok barang : "+stock);
+    }
 }
---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        Barang brg = new Barang("bakso", 25000.0, 10);
+        brg.dispSt();
+    }
+}
