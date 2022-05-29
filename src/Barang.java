e53d629d60b451016d02834f9a32a8de328f4cbf
zahwabmz1562 <zahwafadilla1562@gmail.com>
2022-05-29 13:14:29 +0700

    wawa cantik

--- src/Barang.java
+++ src/Barang.java
@@ -1,4 +1,39 @@
 public class Barang {
-    private String Nama;
+    private String NamaBarang ;
+    private Double Tanggal ;
+    private Double harga ;
+    private int Stok ;
+
+    public void setNamaBarang(String namaBarang) {
+        NamaBarang = namaBarang;
+    }
+
+    public void setTanggal(Double tanggal) {
+        Tanggal = tanggal;
+    }
+
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+
+    public void setStok(int stok) {
+        Stok = stok;
+    }
+
+    public String getNamaBarang() {
+        return NamaBarang;
+    }
+
+    public Double getTanggal() {
+        return Tanggal;
+    }
+
+    public Double getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return Stok;
+    }
 
 }
