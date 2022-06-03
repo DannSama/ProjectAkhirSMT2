deb1a83a0a5a7e117fcb5d286ce66e7d34e2171c
DannSama <axedrill.13@gmail.com>
2022-06-03 16:34:27 +0800

    masuk barang
    ini kelupaan

--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 
 public class main {
     public static void main(String[] args) {
-
+        BarangKeluar brkl = new BarangKeluar("bakso", 2205.0, 25000.0, 10);
+        brkl.BarangKeluar(3, 3);
     }
 
 }
